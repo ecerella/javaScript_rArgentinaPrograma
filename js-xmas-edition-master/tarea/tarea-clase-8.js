a63e7ec71ba2a0e5b97617c15f034627b8c83719
ecerella <ecerella@gmail.com>
2024-08-20 17:22:24 -0300

    juego simon de fabricio explicacion de la clase10, manejo de dom, bootstrap, funciones, etc

--- js-xmas-edition-master/tarea/tarea-clase-8.js
+++ js-xmas-edition-master/tarea/tarea-clase-8.js
@@ -1,7 +1,8 @@
 /*# Tarea clase 8
 
 A las 2 tareas de la clase 6, ponerles las validaciones que consideren
-necesarias.
+necesarias. (regExp, objetos, forEach, poner estilos, mostrar errores
+en la interface de usuario y escribir pruebas)
 
 TIP: Las edades no pueden tener decimales.
 */
